e7c269af255badb764867de81f7bc5673fbe8fc7
Taufiq Okta Pratama Putra <TOPPcomptech@gmail.com>
2016-10-11 09:50:42 +0700

    Ketinggalan Commit Minor Order Page
    Lupa ganti judul sama title nama

--- resources/views/frontend/transaction/order.blade.php
+++ resources/views/frontend/transaction/order.blade.php
@@ -11,7 +11,7 @@
   <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
   <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
   <![endif]-->
-  <title>List Product</title>
+  <title>Order Page</title>
 
   <link href="{{ URL::asset('assets/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css">
   <link href="{{ URL::asset('assets/css/font.css') }}" rel="stylesheet" type="text/css">
@@ -90,7 +90,7 @@
           {!! Form::hidden('fg_code', $product['fg_code'],array('id'=>'fg_code')) !!}
         </div>
         <div class="form-group">
-          {!! Form::label('Nama') !!}
+          {!! Form::label('Nama Sesuai Identitas') !!}
           {!! Form::text('name',null,array('required','class'=>'form-control','placeholder'=>'Nama Anda')) !!}
         </div>
         <div class='form-group'>
